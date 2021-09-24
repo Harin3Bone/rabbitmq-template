32598268a0385aa27a72c4e9d17e5f10ad547745
Harin3Bone <harin.thananam@gmail.com>
2021-09-24 11:20:50 +0700

    Compose - update default value of variable

--- quick-start.sh
+++ quick-start.sh
@@ -1,28 +1,2 @@
 #!/bin/bash
-
-echo "Creating RabbitMQ environment..."
-
-cp default.env .env
-
-for i in 1 2 3
-do 
-    echo "."
-    sleep 1
-done
-
-echo "Starting RabbitMQ server..."
-
 docker-compose up -d
-
-for i in 1 2 3 
-do
-    echo "."
-    sleep 1
-done
-
-echo "RabbitMQ server running http://localhost:5672"
-echo "RabbitMQ management running http://localhost:15672"
-echo "username: root"
-echo "password: password"
-
-sleep 1
